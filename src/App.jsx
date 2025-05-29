7718e0c915cfa672cb66ee559de7526d8284b3ad
Sachin Jaiswal <sj586997@gmail.com>
2025-05-29 14:28:17 +0530

    analytic

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { Allroutes } from './routes/Allroutes';
 import Footer from './components/Footer';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
+
 
 function App() {
   return (
