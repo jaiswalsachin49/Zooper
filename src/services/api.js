8a30328905f5f97a61f1b7e395af7fed9a27b653
Sachin Jaiswal <sj586997@gmail.com>
2025-07-30 15:57:30 +0530

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 const API_KEY = import.meta.env.VITE_API_KEY;
-console.log('API_KEY:', API_KEY); // Debugging line to check if API_KEY is loaded correctly
 
 export default axios.create({
     baseURL: 'https://api.themoviedb.org/3',
